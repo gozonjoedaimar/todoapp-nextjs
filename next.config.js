69222e70f8be47102182f48e9e22fce8e7b06656
gozonjoedaimar <gozonjoedaimar@gmail.com>
2023-02-16 02:05:41 +0800

    Add dev dependencies and env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
+let toEnv = {...process.env};
+delete toEnv.NODE_ENV;
+delete toEnv.NEXT_RUNTIME;
+delete toEnv.__NEXT_PROCESSED_ENV;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: toEnv,
 }
 
 module.exports = nextConfig
